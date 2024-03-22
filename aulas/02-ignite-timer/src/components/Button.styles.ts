61b2e11548790f4019673fd93c65fce4a86111f8
Henrique <henriq2303@gmail.com>
2024-03-21 21:04:52 -0300

    defining the application structure

---
+++ aulas/02-ignite-timer/src/components/Button.styles.ts
@@ -0,0 +1,28 @@
+import styled, { css } from "styled-components";
+
+export type ButtonVariant = 'primary' | 'secundary' | 'danger' | 'success';
+
+interface ButtonContainerProps {
+    variant: ButtonVariant;
+}
+
+const buttonVariants =  {
+    primary: 'purple',
+    secundary: 'orange',
+    danger: 'red',
+    success: 'grenn'
+}
+
+export const ButtonContainer = styled.button<ButtonContainerProps>`
+    width: 100px;
+    height: 40px;
+
+    background-color: ${props => props.theme["green-500"]};
+    color: ${props => props.theme.white};
+
+    /* ${props => {
+        return css `
+            background: ${buttonVariants[props.variant]}
+        `
+    }} */
+`
