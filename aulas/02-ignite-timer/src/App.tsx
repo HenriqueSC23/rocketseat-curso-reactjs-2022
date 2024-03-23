9ba0d474fe3a44b94fbaaaa3c703e9e5c1e05358
Henrique <henriq2303@gmail.com>
2024-03-23 09:23:16 -0300

    adding routes with react-router-dom

--- aulas/02-ignite-timer/src/App.tsx
+++ aulas/02-ignite-timer/src/App.tsx
@@ -1,14 +1,15 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secundary" />
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
