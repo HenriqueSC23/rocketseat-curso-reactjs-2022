6bce6a4f2c722c79255bb25fea0e210b3c606b78
Henrique <henriq2303@gmail.com>
2024-03-30 19:22:31 -0300

    improving inputs

--- aulas/02-ignite-timer/src/pages/Home/index.tsx
+++ aulas/02-ignite-timer/src/pages/Home/index.tsx
@@ -16,13 +16,24 @@ export function Home() {
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
+            list="task-suggestions"
             type="text"
             id="task"
             placeholder="Dê um nome para o seu projeto"
           />
 
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="batata" />
+          </datalist>
+
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
+            step={5}
+            min={5}
+            max={60}
             type="number"
             id="minutesAmount"
             placeholder="00"
