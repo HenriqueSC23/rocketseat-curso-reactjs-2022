cd31a867ca20597f2d91024d052913071c9b644a
Henrique <henriq2303@gmail.com>
2024-05-12 18:28:22 -0300

    add success page

--- aulas/02-coffee-delivery/src/pages/Success/index.tsx
+++ aulas/02-coffee-delivery/src/pages/Success/index.tsx
@@ -1,7 +1,57 @@
-import { Header } from '../../components/Header'
+import { GridContainer, Order, OrderBox, OrderInfo } from './style'
+import Biker from '../../assets/Illustration.png'
+import { CurrencyDollar, MapPin, Timer } from '@phosphor-icons/react'
+import { useTheme } from 'styled-components'
 
 export function Success() {
+  const theme = useTheme()
+
   return (
-  <h1>success</h1>
+    <GridContainer>
+      <Order>
+        <h1>Uhu! Pedido confirmado</h1>
+        <p>Agora é só aguardar que logo o café chegará até você</p>
+
+        <OrderBox>
+          <OrderInfo>
+            <div>
+              <MapPin
+                weight="fill"
+                size={32}
+                color={theme.colors.white}
+                style={{ backgroundColor: theme.colors.purple }}
+              />
+              <span>
+                Entrega em{' '}
+                <strong>Rua João Daniel Martinelli, 102 Farrapos</strong>- Porto
+                Alegre, RS
+              </span>
+            </div>
+            <div>
+              <Timer
+                weight="fill"
+                size={32}
+                color={theme.colors.white}
+                style={{ backgroundColor: theme.colors.yellow }}
+              />
+              <span>
+                Previsão de entrega <strong>20 min - 30 min</strong>
+              </span>
+            </div>
+            <div>
+              <CurrencyDollar
+                size={32}
+                color={theme.colors.white}
+                style={{ backgroundColor: theme.colors['yellow-dark'] }}
+              />
+              <span>
+                Pagamento na entrega <strong>Cartão de Crédito</strong>
+              </span>
+            </div>
+          </OrderInfo>
+        </OrderBox>
+      </Order>
+      <img src={Biker} alt="Motoqueiro delivery" />
+    </GridContainer>
   )
 }
--- aulas/02-coffee-delivery/src/pages/Success/style.ts
+++ aulas/02-coffee-delivery/src/pages/Success/style.ts
@@ -3,11 +3,18 @@ import { defaultTheme } from '../../styles/themes/default'
 
 export const GridContainer = styled.div`
   display: flex;
+  margin: 0 auto;
   padding: 80px 160px;
-  gap: 102px;
+  align-items: flex-end;
+  justify-content: space-between;
+  gap: 40px;
+
+  img {
+    margin-bottom: -13px;
+  }
 `
 
-export const LeftColumn = styled.div`
+export const Order = styled.div`
   h1 {
     ${defaultTheme.fonts.titleL}
     color: ${defaultTheme.colors['yellow-dark']};
@@ -18,5 +25,38 @@ export const LeftColumn = styled.div`
     ${defaultTheme.fonts.textL}
     color: ${defaultTheme.colors['base-subtitle']};
     margin-bottom: 40px;
+  }
+`
+
+export const OrderBox = styled.div`
+  width: 100%;
+  border: 1px solid;
+  border-radius: 6px 36px;
+  border-color: transparent;
+  background-origin: border-box;
+  background-image: linear-gradient(
+    to bottom right,
+    ${defaultTheme.colors.yellow},
+    ${defaultTheme.colors.purple}
+  );
+`
+
+export const OrderInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+  padding: 40px;
+  background-color: ${defaultTheme.colors.white};
+  border-radius: 6px 36px;
+
+  div {
+    display: flex;
+    gap: 12px;
+    align-items: center;
+
+    svg {
+      padding: 8px;
+      border-radius: 50%;
+    }
   }
 `
