f919d7644d52f1380d70779c071a10e2f5058db0
Henrique <henriq2303@gmail.com>
2024-07-25 07:01:33 -0300

    Connecting to the API - Configuring JSON Server

--- aulas/03-dt-money/src/components/NewTransactionModal/index.tsx
+++ aulas/03-dt-money/src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,6 @@
 import * as Dialog from '@radix-ui/react-dialog'
-import { Content, Overlay } from './styles'
+import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles'
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
 
 export function NewTransactionModal() {
   return (
@@ -7,15 +8,26 @@ export function NewTransactionModal() {
       <Overlay />
 
       <Content>
+        <CloseButton><X size={24}/></CloseButton>
+
         <Dialog.Title>Nova Transação</Dialog.Title>
           <form action="">
             <input type="text" placeholder='Descrição' />
             <input type="number" placeholder='Preço' />
             <input type="text" placeholder='Categoria' />
+            <TransactionType>
+              <TransactionTypeButton variant='income' value='income'>
+                <ArrowCircleUp size={24} />
+                Entrada
+              </TransactionTypeButton>
+              <TransactionTypeButton variant='outcome' value='outcome'>
+                <ArrowCircleDown size={24} />
+                Saída
+              </TransactionTypeButton>
+            </TransactionType>
 
             <button type="submit">Cadastrar</button>
           </form>
-        <Dialog.Close />
       </Content>
     </Dialog.Portal>
   )
