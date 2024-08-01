fade5fe122a5a010ab0ca20a933bf95a8d1e27f5
Henrique <henriq2303@gmail.com>
2024-08-01 20:46:52 -0300

    Explanation of the use of memo

--- aulas/03-dt-money/src/pages/Transactions/components/SeachForm/index.tsx
+++ aulas/03-dt-money/src/pages/Transactions/components/SeachForm/index.tsx
@@ -6,6 +6,27 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { TransactionsContext } from '../../../../contexts/TransactionsContext'
 import { useContextSelector } from 'use-context-selector'
 
+/* Por que que um componente renderiza?
+ * 1- Hooks changed (mudou estado, contexto, reducer);
+ * 2- Props changed (mudou propriedades);
+ * 3- Parent rerendered (Componente pai foi renderizado);
+ *
+ * Qual o fluxo de renderização?
+ * 1- O react recria o HTML da interface daquele componente;
+ * 2- Compara a versão do HTML com a anterior;
+ * 3- SE alguma coisa mudou, ele recria o HTML em tela;
+ *
+ * Memo:
+ * 0- Hooks changed, props changed (deep comparison | comparação profunda);
+ * 0.1- Comparar com a versão anterior de hooks e props;
+ * 0.2- SE mudou algo, ele permite a renderização;
+ *
+ * Sintaxe do memo: mude o nome do componente e crie um novo da seguinte forma:
+ * export const SearchForm = memo(SerchFormComponent);
+ *
+ * Isso se aplica para funções no caso de variáveis se usa o useMemo()
+ */
+
 const searchFormSchema = z.object({
   query: z.string(),
 })
